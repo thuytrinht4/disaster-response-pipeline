fa4ca39ab2e67ddba5855413f55bf1e42ad0297d
Thuy Trinh <thuytrinht4@gmail.com>
2021-10-15 02:05:49 +0700

    customer transformer

--- models/custom_transformer.py
+++ models/custom_transformer.py
@@ -7,14 +7,8 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.base import BaseEstimator, TransformerMixin
 
-url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-
 
 def tokenize(text):
-    detected_urls = re.findall(url_regex, text)
-    for url in detected_urls:
-        text = text.replace(url, "urlplaceholder")
-
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -33,7 +27,7 @@ class StartingVerbExtractor(BaseEstimator, TransformerMixin):
         for sentence in sentence_list:
             pos_tags = nltk.pos_tag(tokenize(sentence))
             first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+            if first_tag in ['VB', 'VBP', 'VBG'] or first_word == 'RT':
                 return True
         return False
 
