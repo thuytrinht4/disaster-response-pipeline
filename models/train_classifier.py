04b79294b0be8b487043e78d6a486702dd1f1741
Thuy Trinh <thuytrinht4@gmail.com>
2021-10-15 10:51:03 +0700

    reduce parameters for Gridsearch to cut down training time

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -91,9 +91,7 @@ def build_model():
     parameters = {'features__text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
                   'features__text_pipeline__tfidf__use_idf': (True, False),
                   'features__transformer_weights': (
-                        {'text_pipeline': 1, 'starting_verb': 0.5},
-                        {'text_pipeline': 0.5, 'starting_verb': 1},
-                        {'text_pipeline': 0.8, 'starting_verb': 1})
+                        {'text_pipeline': 1, 'starting_verb': 0.5})
                   }
     cv = GridSearchCV(pipeline, param_grid=parameters)
 
